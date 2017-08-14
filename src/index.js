5dfe7bf8c3f27e7cdbce117f6f3374f96b72a884
Oscar-ren <rxl719704258@gmail.com>
2017-08-14 19:17:31 +0800

    add eslint

---
+++ src/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let foo = {
+  name : "Joy"
+}
+
+let arr = new Array(1, 2, 3, 5);
+arr.push(6)
+
+if(arr.length == 5){
+  //...
+}
+
+else {
+  //...
+}
+
+function func () {
+  return 1;
+  location.href = "http://renxianglei.com";
+}
+
+
+
